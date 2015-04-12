ac68825815798e556a0beaa5cc3eafd6cf085d4b
IanGallacher <igallacher@outlook.com>
2015-04-11 20:15:19 -0700

    Prototype1

---
+++ Assets/Scripts/FileIO.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System;
+using System.Text;
+using System.IO;  
+
+//code from Drakestar  http://answers.unity3d.com/questions/279750/loading-data-from-a-txt-file-c.html
+public class FileIO  {
+	public Word[] Load(string fileName) 
+	{
+		Word[] returnvals = new Word[50];
+		// Handle any problems that might arise when reading the text
+		try
+		{
+			string line;
+			// Create a new StreamReader, tell it which file to read and what encoding the file
+			// was saved as
+			StreamReader theReader = new StreamReader(fileName, Encoding.Default);
+			
+			// Immediately clean up the reader after this block of code is done.
+			// You generally use the "using" statement for potentially memory-intensive objects
+			// instead of relying on garbage collection.
+			// (Do not confuse this with the using directive for namespace at the 
+			// beginning of a class!)
+			using (theReader)
+			{
+				// While there's lines left in the text file, do this:
+				int i = 0;
+				do
+				{
+					line = theReader.ReadLine();
+					
+					if (line != null)
+					{
+						// Do whatever you need to do with the text line, it's a string now
+						// In this example, I split it into arguments based on comma
+						// deliniators, then send that array to DoStuff()
+						string[] entries = line.Split();
+						returnvals[i] = new Word(entries[0], Convert.ToInt32( entries[1].Trim()) );
+					}
+					i++;
+				}
+				while (line != null);
+				
+				// Done reading, close the reader and return true to broadcast success    
+
+				theReader.Close();
+				return (Word[]) returnvals.Clone();
+			}
+		}
+		
+		// If anything broke in the try block, we throw an exception with information
+		// on what didn't work
+		catch (Exception e)
+		{
+			return null;
+		}
+	}
+}
---
+++ Assets/Scripts/RotatingCube.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotatingCube : MonoBehaviour {
+	void Update () {
+		Vector3 a = gameObject.transform.eulerAngles;
+		gameObject.transform.eulerAngles = new Vector3 (a.x, a.y + 1, a.z);
+	}
+}
